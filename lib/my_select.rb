562eab26d5612163e4c084bef79cc20f3f512efb
github username <github email address>
2019-06-13 23:04:34 +0100

    select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
+# def my_select(collection)
+ 
+#  collection.select {|num| num.even?}
+
+
+# end
+
+
 def my_select(collection)
- # your code here!
+ 
+  i = 0
+  select = []
+  while i < collection.length
+    if yield(collection[i])
+      select << collection[i]
+    end
+    i+=1
+  end
+  select
 end
